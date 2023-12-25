d145155321aa6afa1125133b894f2cd415415cad
MilkeeyCat <40633857+MilkeeyCat@users.noreply.github.com>
2023-12-25 21:16:48 +0200

    feat: show messages sent before

--- internal/database/message.go
+++ internal/database/message.go
@@ -7,6 +7,14 @@ type Message struct {
 	CreatedAt string `json:"created_at"`
 }
 
+type MessageWithUser struct {
+	Id        int    `json:"id"`
+	Username  string `json:"username"`
+	UserId    int    `json:"user_id"`
+	Message   string `json:"message"`
+	CreatedAt string `json:"created_at"`
+}
+
 // NOTE: -1 means public room
 func CreateMessage(userId int, message string, roomId int) error {
 	_, err := Db.Exec("INSERT INTO messages (user_id, message, room_id) VALUES (?, ?, ?)", userId, message, roomId)
@@ -15,4 +23,37 @@ func CreateMessage(userId int, message string, roomId int) error {
 	}
 
 	return nil
+}
+
+func GetRoomMessagesInRoom(roomId int) ([]MessageWithUser, error) {
+	messages := []MessageWithUser{}
+
+	rows, err := Db.Query("SELECT id, user_id, message, created_at FROM messages WHERE room_id = ?", roomId)
+	if err != nil {
+		return []MessageWithUser{}, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		msg := Message{}
+
+		if err := rows.Scan(&msg.Id, &msg.UserId, &msg.Message, &msg.CreatedAt); err != nil {
+			return []MessageWithUser{}, err
+		}
+
+		usr, err := FindUserById(msg.UserId)
+		if err != nil {
+			return []MessageWithUser{}, err
+		}
+
+		messages = append(messages, MessageWithUser{
+			Id:        msg.Id,
+			Username:  usr.Name,
+			UserId:    msg.UserId,
+			Message:   msg.Message,
+			CreatedAt: msg.CreatedAt,
+		})
+	}
+
+	return messages, nil
 }
--- internal/handlers/common.go
+++ internal/handlers/common.go
@@ -1,15 +1,24 @@
 package handler
 
 import (
+	"github.com/MilkeeyCat/conversa/internal/database"
 	"github.com/MilkeeyCat/conversa/views/pages"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
 
 func Index(c echo.Context) error {
-	isAuthed := c.Get("user") != nil
+	claims := c.Get("user")
 
-	if isAuthed {
-		return render(c, pages.Authed())
+	if claims != nil {
+		data := claims.(*jwt.Token).Claims.(*JwtCustomClaims)
+		messages, err := database.GetRoomMessagesInRoom(-1)
+		if err != nil {
+			c.Logger().Error(err)
+			return err
+		}
+
+		return render(c, pages.Authed(messages, data.Id))
 	}
 
 	return render(c, pages.Guest())
